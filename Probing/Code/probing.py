955db21594210a6415649c302e979c03c5b56e3d
Barry Wang <33831597+GithuBarry@users.noreply.github.com>
2023-03-26 20:51:28 -0400

    Update probing.py

--- Probing/Code/probing.py
+++ Probing/Code/probing.py
@@ -2,81 +2,86 @@ from abc import ABC
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import seaborn as sn
 import torch
-from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+
+class LinearLayerClassification(torch.nn.Module, ABC):
+    def __init__(self, input_dimension):
+        super().__init__()
+        self.linear = torch.nn.Linear(input_dimension, 2)
+
+    def forward(self, input_dimension):
+        return self.linear(input_dimension)
+
+
+def configure_loss_function():
+    return torch.nn.MSELoss()
+
+
+def configure_optimizer(model):
+    return torch.optim.Adam(model.parameters())
+
+
+def full_gd(model, criterion, optimizer, X_train, y_train, n_epochs=2000):
+    train_losses = np.zeros(n_epochs)
+    test_losses = np.zeros(n_epochs)
+
+    for it in range(n_epochs):
+        outputs = model(X_train)
+        loss = criterion(outputs, y_train)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        outputs_test = model(X_test)
+        loss_test = criterion(outputs_test, y_test)
+
+        train_losses[it] = loss.item()
+        test_losses[it] = loss_test.item()
+
+        if (it + 1) % 50 == 0:
+            print(
+                f'In this epoch {it + 1}/{n_epochs}, Training loss: {loss.item():.4f}, Test loss: {loss_test.item():.4f}')
+
+    return train_losses, test_losses
+
+
 if __name__ == "__main__":
 
+    if torch.cuda.is_available():
+        print("Setting CUDA as default")
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+    print("loading X Y")
     X = np.load(
         "../X/X_GTT_layer_last_bert-uncased_epoch20_muc1700.npy")
     Y = np.load(
         "../Y/Y_muc_1700_num_events.npy")
 
+    print("Reshaping X Y")
     Y = Y.reshape(-1, 1)
     X = X.reshape(1700, -1)
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
 
+    print("Scale-fitting X Y")
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
 
-
-    class LinearLayerClassification(torch.nn.Module, ABC):
-        def __init__(self, input_dimension):
-            super().__init__()
-            self.linear = torch.nn.Linear(input_dimension, 2)
-
-        def forward(self, input_dimension):
-            return self.linear(input_dimension)
-
+    print("Torching X Y")
+    X_train = torch.from_numpy(X_train.astype(np.float32))
+    X_test = torch.from_numpy(X_test.astype(np.float32))
+    y_train = torch.from_numpy(y_train.astype(np.float32))
+    y_test = torch.from_numpy(y_test.astype(np.float32))
 
     _, input_dimension = X_train.shape
     _, output_dimension = Y.shape
 
     model = torch.nn.Linear(input_dimension, output_dimension)
 
-    """train the model"""
-    def configure_loss_function():
-        return torch.nn.MSELoss()
-
-
-    def configure_optimizer(model):
-        return torch.optim.Adam(model.parameters())
-
-
-    def full_gd(model, criterion, optimizer, X_train, y_train, n_epochs=2000):
-        train_losses = np.zeros(n_epochs)
-        test_losses = np.zeros(n_epochs)
-
-        for it in range(n_epochs):
-            outputs = model(X_train)
-            loss = criterion(outputs, y_train)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            outputs_test = model(X_test)
-            loss_test = criterion(outputs_test, y_test)
-
-            train_losses[it] = loss.item()
-            test_losses[it] = loss_test.item()
-
-            if (it + 1) % 50 == 0:
-                print(
-                    f'In this epoch {it + 1}/{n_epochs}, Training loss: {loss.item():.4f}, Test loss: {loss_test.item():.4f}')
-
-        return train_losses, test_losses
-
-
-    X_train = torch.from_numpy(X_train.astype(np.float32))
-    X_test = torch.from_numpy(X_test.astype(np.float32))
-    y_train = torch.from_numpy(y_train.astype(np.float32))
-    y_test = torch.from_numpy(y_test.astype(np.float32))
-
+    print("Training X Y")
     criterion = configure_loss_function()
     optimizer = configure_optimizer(model)
     train_losses, test_losses = full_gd(model, criterion, optimizer, X_train, y_train)
@@ -107,11 +112,12 @@ if __name__ == "__main__":
     for inputs, labels in [(X_test[i], y_test[i]) for i in range(len(X_test))]:
         output = model(inputs)  # Feed Network
 
-        # output = (torch.max(torch.exp(output), 1)[1]).data.cpu().numpy()
-        y_pred.extend(output)  # Save Prediction
+        y_pred.extend(output.cpu().detach().numpy())  # Save Prediction
 
         labels = labels.data.cpu().numpy()
         y_true.extend(labels)  # Save Truth
+
+        print("y_pred", y_pred)
     pass
 
     # constant for classes
