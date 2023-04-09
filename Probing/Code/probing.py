a40aee62dee7f3524c6115c861fc1188654f3a1f
Barry Wang <33831597+GithuBarry@users.noreply.github.com>
2023-04-09 14:41:49 -0400

    Adding `relu`

--- Probing/Code/probing.py
+++ Probing/Code/probing.py
@@ -1,13 +1,14 @@
+import json
+import os
 from abc import ABC
 
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from sklearn.model_selection import train_test_split
-import os
 from sklearn.preprocessing import StandardScaler
 
-import json
+
 class LinearLayerClassification(torch.nn.Module, ABC):
     def __init__(self, input_dimension, output_dimension):
         super().__init__()
@@ -21,12 +22,12 @@ class LinearLayerClassification(torch.nn.Module, ABC):
 
 def configure_loss_function():
     return torch.nn.MSELoss()
-    #return torch.nn.L1Loss()
+    # return torch.nn.L1Loss()
 
 
 def configure_optimizer(model):
     return torch.optim.Adam(model.parameters())
-    #return torch.optim.SGD(model.parameters(), lr=0.01)
+    # return torch.optim.SGD(model.parameters(), lr=0.01)
 
 
 def full_gd(model, criterion, optimizer, X_train, y_train, n_epochs):
@@ -56,18 +57,18 @@ def full_gd(model, criterion, optimizer, X_train, y_train, n_epochs):
 if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     epoch = 2000
-    for x in ["X_dygiepp_embedding-unpadded_bert-uncased_epoch21_muc1700.npy"]:# os.listdir("../X/"):
+    for x in os.listdir("../X/"):
         for y in os.listdir("../Y/"):
-            if x[-4:] != ".npy" or y[-4:] != ".npy" or "5_" in x or "2_" in x or "3_" in x or "4_" in x or "6_" in x or  "7_" in x or  "8_" in x or  "9_" in x or  "10_" in x or  "11_" in x:
+            if x[-4:] != ".npy" or y[-4:] != ".npy":
                 continue
-            print("Running on:",x,y)
+            print("Running on:", x, y)
             x_name = x[:-4]
             y_name = y[:-4]
-            #x_name = "X_TANL_layer_last_bert-uncased_epoch20_muc1700"
-            #y_name = "Y_muc_1700_num_events"
+            # x_name = "X_TANL_layer_last_bert-uncased_epoch20_muc1700"
+            # y_name = "Y_muc_1700_num_events"
             print("loading X Y")
             X = np.load(
-                f"../X/{x_name}.npy",allow_pickle=True)
+                f"../X/{x_name}.npy", allow_pickle=True)
             Y = np.load(
                 f"../Y/{y_name}.npy")
 
@@ -77,11 +78,9 @@ if __name__ == "__main__":
             lengths = [len(x) for x in X]
             for i in range(len(X)):
                 if len(X[i]) < max(lengths):
-                    np.append(X[i], [0]* (max(lengths) - len(X[i])))
+                    np.append(X[i], [0] * (max(lengths) - len(X[i])))
 
             X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
-            
-            
 
             print("Scale-fitting X Y")
             scaler = StandardScaler()
@@ -131,31 +130,17 @@ if __name__ == "__main__":
 
             # iterate over test data
             for inputs, labels in [(X_test[i], y_test[i]) for i in range(len(X_test))]:
-                output = model(inputs)  # Feed Network
+                output = torch.relu(model(inputs))  # Feed Network
                 y_pred.extend(output.cpu().detach().numpy())  # Save Prediction
                 labels = labels.data.cpu().detach().numpy()
                 y_true.extend(labels)  # Save Truth
                 pass
 
-            result = {"y_pred":[float(y) for y in y_pred],"y_pred_int": [int(np.rint(y).astype(int)) for y in y_pred], "y_true": [int(y.astype(int)) for y in y_true], "train_acc":float(train_acc), "test_acc":float(test_acc), "X": x_name, "Y":y_name}
+            result = {"y_pred": [float(y) for y in y_pred], "y_pred_int": [int(np.rint(y).astype(int)) for y in y_pred],
+                      "y_true": [int(y.astype(int)) for y in y_true], "train_acc": float(train_acc),
+                      "test_acc": float(test_acc), "X": x_name, "Y": y_name}
             print(result)
-            
+
             with open(f'result_{y_name}_{x_name}_epoch{epoch_str}.json', 'w') as f:
                 json.dump(result, f, indent=4)
             pass
-
-            # constant for classes
-            # classes = sorted(list(set(y_true).union(set(y_pred))))
-
-            # Build confusion matrix
-            # cf_matrix = confusion_matrix(y_true, y_pred)
-            # cf_matrix = confusion_matrix([enc.inverse_transform(y) for y in y_true],
-            #                              np.array([enc.inverse_transform(y.detach()) for y in y_pred]).astype(np.int),
-            #                              labels=enc.categories_[0])
-            # df_cm = pd.DataFrame(cf_matrix, index=enc.categories_[0],
-            #                      columns=enc.categories_[0])
-            # plt.figure(figsize=(12, 11))
-            # plt.xlabel = "Pred"
-            # plt.ylabel = "True"
-            # sn.heatmap(df_cm, annot=True)
-            # plt.savefig('output_entities.png')
