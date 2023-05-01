2afad8cb879c17d9f7154f3f522eaac50ca74bf1
Barry Wang <33831597+GithuBarry@users.noreply.github.com>
2023-04-30 22:56:30 -0400

    Updated Graphs

--- Probing/Code/confusion_matrix.py
+++ Probing/Code/confusion_matrix.py
@@ -6,18 +6,25 @@ import numpy as np
 from sklearn import metrics
 
 if __name__ == '__main__':
-    for file in os.listdir():
-        prefix = "result_"
-        if file[:len(prefix)] == prefix and file[-5:] == ".json" and ("events" in file or "num_sent" in file):
-            print(file)
-            with open(file) as f:
-                result = json.load(f)
+    results_path = "../Results-MLPwAttention-Bucketed/"
+    for file in os.listdir(results_path):
+        #####Filtering#####
+        if "nhid400" not in file:
+            continue
+        ###################
 
-            confusion_matrix = metrics.confusion_matrix(np.array(result["y_true"]), np.array(result["y_pred_int"]))
+        prefix = "probresult"
+        if file[:len(prefix)] == prefix and file[-5:] == ".json":
+            print(os.path.join(results_path, file))
+            with open(os.path.join(results_path, file)) as f:
+                result = json.load(f)
+            labels = np.array([l.index(max(l)) for l in result["val_true"]])
+            preds = np.array([l.index(max(l)) for l in result["val_pred"]])
+            confusion_matrix = metrics.confusion_matrix(labels, preds)
             print(confusion_matrix)
 
-            cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix, display_labels=sorted(
-                list(set(result["y_true"] + result["y_pred_int"]))))
+            cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix,
+                                                        display_labels=sorted(list(set(list(labels) + list(preds)))))
 
             cm_display.plot()
             plt.savefig(file[:-5] + ".png")
