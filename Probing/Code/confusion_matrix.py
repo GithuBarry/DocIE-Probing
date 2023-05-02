304e6bcbbdbe61ca5a959681938b5ec8373cf1f6
Barry Wang <33831597+GithuBarry@users.noreply.github.com>
2023-05-02 15:13:05 -0400

    Adding graphs

--- Probing/Code/confusion_matrix.py
+++ Probing/Code/confusion_matrix.py
@@ -6,7 +6,7 @@ import numpy as np
 from sklearn import metrics
 
 if __name__ == '__main__':
-    results_path = "../Results-MLPwAttention-BucketedRun2/"
+    results_path = "../Results-MLPwAttention-Bucketed/"
     for file in os.listdir(results_path):
         #####Filtering#####
         if "nhid400" not in file:
